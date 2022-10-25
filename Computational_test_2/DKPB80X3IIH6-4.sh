eee77d5fe9ceb98ab8922c89f2ad791b69688168
lsx1980 <suxingliu@gmail.com>
2022-10-25 14:28:19 -0400

    sort scripts and add all scripts

---
+++ Computational_test_2/DKPB80X3IIH6-4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=bucksch_p
+#SBATCH --gres=gpu:V100S:1
+#SBATCH --ntasks=1
+#SBATCH --mem=64GB
+#SBATCH --time=04:00:00
+#SBATCH --export=NONE
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=sl02494@uga.edu
+
+cd $SLURM_SUBMIT_DIR
+
+cd /scratch/sl02494/molly_root/DKPB80X3IIH6-4/
+
+singularity exec --nv docker://computationalplantscience/dirt3d-reconstruction python3 /opt/code/pipeline.py -i /scratch/sl02494/molly_root/DKPB80X3IIH6-4/images/ -o . -g 1 -d COLMAP
